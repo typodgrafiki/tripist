65888eebb4e10b74da7f917fc3c4d72ca7cb65f9
typodgrafiki <53211722+typodgrafiki@users.noreply.github.com>
2023-09-13 15:25:56 +0200

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import './globals.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Trip lists',
+  description: 'Manage your trip lists',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      Hello World in Trip list
+    </main>
+  )
+}
