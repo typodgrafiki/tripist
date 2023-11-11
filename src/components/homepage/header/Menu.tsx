1c747886db7a1a62165947e58d34eea0806161d0
typodgrafiki <grzegorz.kocik.pn@gmail.com>
2023-11-11 17:23:45 +0100

    fix build

--- src/components/homepage/header/Menu.tsx
+++ src/components/homepage/header/Menu.tsx
@@ -59,13 +59,13 @@ function Menu() {
                         className="h-8 w-8"
                         fill="none"
                         viewBox="0 0 24 24"
-                        stroke-width="2"
+                        strokeWidth="2"
                         stroke="currentColor"
                         aria-hidden="true"
                     >
                         <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                             d="M6 18L18 6M6 6l12 12"
                         />
                     </svg>
@@ -81,13 +81,13 @@ function Menu() {
                     className="h-8 w-8"
                     fill="none"
                     viewBox="0 0 24 24"
-                    stroke-width="2"
+                    strokeWidth="2"
                     stroke="currentColor"
                     aria-hidden="true"
                 >
                     <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                         d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
                     />
                 </svg>
