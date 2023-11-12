3b6d4f5fb0df2515774717bb538c0d3b24688e30
typodgrafiki <grzegorz.kocik.pn@gmail.com>
2023-11-12 21:35:29 +0100

    up

--- src/app/api/webhooks/user/route.ts
+++ src/app/api/webhooks/user/route.ts
@@ -3,10 +3,13 @@ import { headers } from "next/headers"
 import { NextResponse } from "next/server"
 import { Webhook, WebhookRequiredHeaders } from "svix"
 import prisma from "@/lib/prismaClient"
+import * as crypto from "crypto"
 
 const webhookSecret = process.env.WEBHOOK_SECRET || ""
 
 async function handler(request: Request) {
+    console.log("success api")
+
     const payload = await request.json()
     const headersList = headers()
     const heads = {
@@ -14,6 +17,26 @@ async function handler(request: Request) {
         "svix-timestamp": headersList.get("svix-timestamp"),
         "svix-signature": headersList.get("svix-signature"),
     }
+
+    if (!heads["svix-signature"] || !heads["svix-timestamp"]) {
+        return false
+    }
+
+    const data = JSON.stringify(payload)
+
+    // Utwórz oczekiwany podpis
+    const expectedSignature = crypto
+        .createHmac("sha256", webhookSecret)
+        .update(heads["svix-timestamp"] + "." + data)
+        .digest("hex")
+
+    // Porównaj oczekiwany podpis z podpisem z nagłówka
+    const rrr = crypto.timingSafeEqual(
+        Buffer.from(heads["svix-signature"]),
+        Buffer.from(expectedSignature)
+    )
+    console.log(rrr)
+
     const wh = new Webhook(webhookSecret)
     let evt: Event | null = null
 
@@ -24,6 +47,7 @@ async function handler(request: Request) {
         ) as Event
 
         const eventType = evt.type
+
         if (eventType === "user.created" || eventType === "user.updated") {
             const { id, email_addresses, first_name, last_name } = evt.data
 
@@ -43,6 +67,7 @@ async function handler(request: Request) {
             return NextResponse.json({}, { status: 200 })
         }
     } catch (err) {
+        console.log("error")
         console.error((err as Error).message)
         return NextResponse.json({}, { status: 400 })
     }
